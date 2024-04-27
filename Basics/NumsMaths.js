9c4188de8ced2d0b344ff13df29d1d1079ccc312
G <79910671+GeetKaushik@users.noreply.github.com>
2024-04-27 13:28:00 +0000

    basics - nums & maths 2

--- Basics/NumsMaths.js
+++ Basics/NumsMaths.js
@@ -1,4 +1,6 @@
 //++++++++++++++`Numbers`+++++++++++++++++++++++++
+//! NOTE: For more info - `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number`
+
 const num1 = 100
 console.log(num1)
 console.log(typeof num1)
@@ -17,6 +19,7 @@ let million = 100000000
 console.log(`I will have a Rs.${million.toLocaleString('en-IN')} in future`)
 
 //++++++++++++++++`Maths`++++++++++++++++
+//! NOTE: For more info - `https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math`
 console.log(Math)
 console.log(Math.abs(-10))
 console.log(Math.round(4.5))
@@ -24,4 +27,10 @@ console.log(Math.ceil(4.5))
 console.log(Math.floor(4.5))
 console.log(Math.min(1, 2, 3, 5, 0, 3))
 console.log(Math.max(1, 2, 3, 5, 0, 3))
-console.log((Math.random()*10).toFixed(0));
+console.log((Math.random() * 10).toFixed(0))
+console.log(Math.floor(Math.random() * 10) + 1) //better and optimized way
+
+const min = 10
+const max = 60
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // Formula to calculate random number between specific min and max
