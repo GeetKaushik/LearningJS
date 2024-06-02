3855c2044d538efe41fdcea397e6ffc84c739f67
G <79910671+GeetKaushik@users.noreply.github.com>
2024-06-02 08:59:47 +0000

    advance - all about prototypes 2

--- Advance/Prototype.js
+++ Advance/Prototype.js
@@ -14,6 +14,52 @@ Object.prototype.Super = (item) => {
   console.log(`I am a super function available everywhere : ${item} was passed`)
 }
 
-myArr.Super(myArr)
-myObj.Super('a')
-myName.Super(myName)
+Array.prototype.SuperArr = (item) => {
+  console.log(`I am a super function available in Array : ${item} was passed`)
+}
+
+// myArr.Super(myArr)
+// myObj.Super('a')
+// myName.Super(myName)
+// myArr.SuperArr(myArr) // Only presents in Array
+
+// Inheritance
+
+const User = {
+  name: 'Geet',
+  age: 20,
+}
+
+const Admin = {
+  isAdmin: true,
+}
+
+const UserInfo = {
+  __proto__: User,
+  isVerified: true,
+}
+
+const AdminInfo = {
+  isWorking: true,
+}
+
+Admin.__proto__ = UserInfo
+
+// console.log(UserInfo.age)
+
+// modern syntax
+Object.setPrototypeOf(AdminInfo, Admin)
+// console.log(AdminInfo.isAdmin)
+
+// Find true length of string without using trim method
+
+let Str1 = 'Geet      '
+let Str2 = 'Kaushik     '
+
+console.log(`Length of ${Str1}: ${Str1.length}`)
+console.log(`Length of ${Str2}: ${Str2.length}`)
+String.prototype.trueLength = function () {
+  console.log(`True Length of ${this}:  ${this.trim().length}`)
+}
+Str1.trueLength()
+Str2.trueLength()
